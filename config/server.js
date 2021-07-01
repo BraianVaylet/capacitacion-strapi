bf208c4a3ee4e699bd7ee1c0c1582055547c13fd
Braian Vaylet <braianvaylet@gmail.com>
2021-06-30 21:29:52 -0300

    start project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ada97d938d70dd59f16023f63150e8af'),
+    },
+  },
+});
